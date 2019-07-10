b8d3b9ad9a09307e7d74fd8561e1c4f4b0bed602
swaroopa <swaroopa@stellentsoft.com>
2019-07-10 10:24:16 +0530

    Added src folder

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+/**
+ * Created by InspireUI on 19/02/2017.
+ *
+ * @format
+ */
+
+import React from "react";
+import { View, StatusBar, NetInfo, I18nManager } from "react-native";
+
+class Router extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <View />;
+  }
+}
+
+export default Router;
